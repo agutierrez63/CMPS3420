0e594d1e3ccd8cd50cc0ae9fdfe6b1b609041680
Adrian Gutierrez <46102354+agutierrez63@users.noreply.github.com>
2020-02-21 14:10:37 -0800

    Starting Point
    A starting point for our front end.

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+require_once("navigation.php");
+$home = new Page();
+$home->pageTitle = "Dollar Tree - Home";
+$home->headerTitle = "Home";
+
+$home->displayPage();
+?>
+<link href='style.css' type='text/css' rel='stylesheet'>
